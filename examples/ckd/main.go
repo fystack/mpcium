0c4cb9ba2cd7c37a161067581aea1afb8203ae48
vietddude <imrbdnv@gmail.com>
2025-10-31 15:00:27 +0700

    Fix example CKD

--- examples/ckd/main.go
+++ examples/ckd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"slices"
 	"syscall"
 
 	"github.com/fystack/mpcium/pkg/client"
@@ -18,9 +19,32 @@ import (
 
 func main() {
 	const environment = "dev"
-	config.InitViperConfig()
+	config.InitViperConfig("")
 	logger.Init(environment, true)
 
+	algorithm := viper.GetString("event_initiator_algorithm")
+	if algorithm == "" {
+		algorithm = string(types.EventInitiatorKeyTypeEd25519)
+	}
+
+	// Validate algorithm
+	if !slices.Contains(
+		[]string{
+			string(types.EventInitiatorKeyTypeEd25519),
+			string(types.EventInitiatorKeyTypeP256),
+		},
+		algorithm,
+	) {
+		logger.Fatal(
+			fmt.Sprintf(
+				"invalid algorithm: %s. Must be %s or %s",
+				algorithm,
+				types.EventInitiatorKeyTypeEd25519,
+				types.EventInitiatorKeyTypeP256,
+			),
+			nil,
+		)
+	}
 	natsURL := viper.GetString("nats.url")
 	natsConn, err := nats.Connect(natsURL)
 	if err != nil {
@@ -29,9 +53,16 @@ func main() {
 	defer natsConn.Drain()
 	defer natsConn.Close()
 
+	localSigner, err := client.NewLocalSigner(types.EventInitiatorKeyType(algorithm), client.LocalSignerOptions{
+		KeyPath: "./event_initiator.key",
+	})
+	if err != nil {
+		logger.Fatal("Failed to create local signer", err)
+	}
+
 	mpcClient := client.NewMPCClient(client.Options{
 		NatsConn: natsConn,
-		KeyPath:  "./event_initiator.key",
+		Signer:   localSigner,
 	})
 
 	// 2) Once wallet exists, immediately fire a SignTransaction
